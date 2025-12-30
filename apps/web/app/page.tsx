5ff74842f1b762cec6e359601096ad1b384fb2c8
Chris Bateson <cbateson@cynerge.com>
2025-12-29 20:23:03 -0500

    updated frontend to handle login vs dashboard

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,11 +2,16 @@ import { redirect } from 'next/navigation';
 import { auth } from '@/lib/auth';
 
 export default async function Home() {
-  const session = await auth();
+	const session = await auth();
 
-  if (session?.user) {
-    redirect('/dashboard');
-  } else {
-    redirect('/login');
-  }
+	if (!session) {
+		console.log('No session found, redirecting to login');
+		redirect('/login');
+	}
+
+	if (session?.user) {
+		redirect('/dashboard');
+	} else {
+		redirect('/login');
+	}
 }
